d8cab86428254cebfa341576572358d94b32b995
Porrawat Teeraparkorn - 6609681322 <134156900+Bossddddd@users.noreply.github.com>
2026-06-05 13:07:06 +0700

    v0.2.9
    insight add

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <SpeedInsights />
         <Toaster 
           position="top-right" 
           toastOptions={{
