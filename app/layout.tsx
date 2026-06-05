f07b2d71f511b12c02621e902cc1d86881ab2d81
Porrawat Teeraparkorn - 6609681322 <134156900+Bossddddd@users.noreply.github.com>
2026-06-05 11:33:07 +0700

    v0.2.7

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "EVT Admin Panel",
   description: "ระบบจัดการซ่อมบำรุงยานพาหนะ",
+  icons: {
+    icon: '/icon.svg',
+  },
 };
 
 export default function RootLayout({
