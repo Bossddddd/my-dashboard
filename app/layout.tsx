b66e4124a6a711ae3b6a6807cc6ae1e23dc149f4
Porrawat Teeraparkorn - 6609681322 <134156900+Bossddddd@users.noreply.github.com>
2026-06-24 10:11:14 +0700

    0.5.18

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import SyncManager from "@/components/SyncManager";
 
-// Test trigger for CI/CD Pipeline
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -25,6 +25,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <SpeedInsights />
+        <SyncManager />
         <Toaster
           position="top-right"
           toastOptions={{
